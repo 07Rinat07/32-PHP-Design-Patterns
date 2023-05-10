a15e285b1f1da14c6dd3263b989dc57f21f1882b
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-11 00:12:06 +0500

    Factory_method

--- factory.php
+++ factory.php
@@ -1,33 +1,33 @@
 <?php
-
-class Worker
-{
-    private string $name;
-
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name
-     */
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-}
-
-class WorkerFactory
-{
-    public static function make(): Worker
-    {
-        return new Worker();
-    }
-}
-$worker = WorkerFactory::make();
-$worker->setName('Boriska');
-var_dump($worker->getName());
+//
+//class Worker
+//{
+//    private string $name;
+//
+//    /**
+//     * @return string
+//     */
+//    public function getName(): string
+//    {
+//        return $this->name;
+//    }
+//
+//    /**
+//     * @param string $name
+//     */
+//    public function setName(string $name): void
+//    {
+//        $this->name = $name;
+//    }
+//}
+//
+//class WorkerFactory
+//{
+//    public static function make(): Worker
+//    {
+//        return new Worker();
+//    }
+//}
+//$worker = WorkerFactory::make();
+//$worker->setName('Boriska');
+//var_dump($worker->getName());
