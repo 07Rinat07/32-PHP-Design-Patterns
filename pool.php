1280bc67efd04eca755423c1d389f2f51ef380a8
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-11 13:55:35 +0500

    Dependency_injection

--- pool.php
+++ pool.php
@@ -1,76 +1,76 @@
 <?php
-
-class WorkerPool
-{
-    private array $freeWorkers = [];
-
-    /**
-     * @return array
-     */
-    public function getFreeWorkers(): array
-    {
-        return $this->freeWorkers;
-    }
-
-    /**
-     * @param array $freeWorkers
-     */
-    public function setFreeWorkers(array $freeWorkers): void
-    {
-        $this->freeWorkers = $freeWorkers;
-    }
-
-    /**
-     * @return array
-     */
-    public function getBusyWorkers(): array
-    {
-        return $this->busyWorkers;
-    }
-
-    /**
-     * @param array $busyWorkers
-     */
-    public function setBusyWorkers(array $busyWorkers): void
-    {
-        $this->busyWorkers = $busyWorkers;
-    }
-    private array $busyWorkers = [];
-
-    public function getWorker():Worker
-    {
-        if (count($this->freeWorkers) === 0) {
-            $worker = new Worker();
-        } else {
-            $worker = array_pop($this->freeWorkers);
-        }
-        $this->busyWorkers[spl_object_hash($worker)] = $worker;
-        return $worker;
-    }
-
-    public function release(Worker $worker)
-    {
-        $key = spl_object_hash($worker);
-
-        if (isset($this->busyWorkers[$key])) {
-            unset($this->busyWorkers[$key]);
-            $this->freeWorkers[$key] = $worker;
-        }
-    }
-}
-
-class Worker
-{
-    public function work()
-    {
-        printf('Im working');
-    }
-}
-
-$pool = new WorkerPool();
-$worker = $pool->getWorker();
-$worker3 = $pool->getWorker();
-$worker4 = $pool->getWorker();
-$pool->release($worker3);
-
-var_dump($pool->getFreeWorkers());
+//
+//class WorkerPool
+//{
+//    private array $freeWorkers = [];
+//
+//    /**
+//     * @return array
+//     */
+//    public function getFreeWorkers(): array
+//    {
+//        return $this->freeWorkers;
+//    }
+//
+//    /**
+//     * @param array $freeWorkers
+//     */
+//    public function setFreeWorkers(array $freeWorkers): void
+//    {
+//        $this->freeWorkers = $freeWorkers;
+//    }
+//
+//    /**
+//     * @return array
+//     */
+//    public function getBusyWorkers(): array
+//    {
+//        return $this->busyWorkers;
+//    }
+//
+//    /**
+//     * @param array $busyWorkers
+//     */
+//    public function setBusyWorkers(array $busyWorkers): void
+//    {
+//        $this->busyWorkers = $busyWorkers;
+//    }
+//    private array $busyWorkers = [];
+//
+//    public function getWorker():Worker
+//    {
+//        if (count($this->freeWorkers) === 0) {
+//            $worker = new Worker();
+//        } else {
+//            $worker = array_pop($this->freeWorkers);
+//        }
+//        $this->busyWorkers[spl_object_hash($worker)] = $worker;
+//        return $worker;
+//    }
+//
+//    public function release(Worker $worker)
+//    {
+//        $key = spl_object_hash($worker);
+//
+//        if (isset($this->busyWorkers[$key])) {
+//            unset($this->busyWorkers[$key]);
+//            $this->freeWorkers[$key] = $worker;
+//        }
+//    }
+//}
+//
+//class Worker
+//{
+//    public function work()
+//    {
+//        printf('Im working');
+//    }
+//}
+//
+//$pool = new WorkerPool();
+//$worker = $pool->getWorker();
+//$worker3 = $pool->getWorker();
+//$worker4 = $pool->getWorker();
+//$pool->release($worker3);
+//
+//var_dump($pool->getFreeWorkers());
