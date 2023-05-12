0b973df59253660fa81f74dc5c9b8fca63b4d0f9
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-12 18:01:46 +0500

    Command

--- behavioral/object_null.php
+++ behavioral/object_null.php
@@ -1,42 +1,42 @@
 <?php
-
-interface Worker
-{
-    public function work();
-}
-class ObjectManager
-{
-    private Worker$worker;
-
-    /**
-     * @param Worker $worker
-     */
-    public function __construct(Worker $worker)
-    {
-        $this->worker = $worker;
-    }
-    public function goWork()
-    {
-        $this->worker->work();
-    }
-}
-class Developer implements Worker
-{
-    public function work()
-    {
-        printf('Developer is working');
-    }
-}
-class NullWorker implements Worker
-{
-    public function work()
-    {
-
-    }
-}
-
-$developer = new Developer();
-$nullableDeveloper = new NullWorker();
-$objectManager = new ObjectManager($nullableDeveloper); //нет вывода ошибки и нет выполнения-нул абельное выполнение
-
-$objectManager->goWork();
+//
+//interface Worker
+//{
+//    public function work();
+//}
+//class ObjectManager
+//{
+//    private Worker$worker;
+//
+//    /**
+//     * @param Worker $worker
+//     */
+//    public function __construct(Worker $worker)
+//    {
+//        $this->worker = $worker;
+//    }
+//    public function goWork()
+//    {
+//        $this->worker->work();
+//    }
+//}
+//class Developer implements Worker
+//{
+//    public function work()
+//    {
+//        printf('Developer is working');
+//    }
+//}
+//class NullWorker implements Worker
+//{
+//    public function work()
+//    {
+//
+//    }
+//}
+//
+//$developer = new Developer();
+//$nullableDeveloper = new NullWorker();
+//$objectManager = new ObjectManager($nullableDeveloper); //нет вывода ошибки и нет выполнения-нул абельное выполнение
+//
+//$objectManager->goWork();
