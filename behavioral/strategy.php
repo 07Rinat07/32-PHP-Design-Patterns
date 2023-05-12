4a3375f04f2e1e2d4ee9f9a113d24274ed9406db
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-12 15:40:53 +0500

    Strategy

---
+++ behavioral/strategy.php
@@ -0,0 +1,61 @@
+<?php
+
+interface Definer
+{
+    public function define($arg): string;
+}
+class Data
+{
+    private Definer $definer;
+    private int|string|bool $arg;
+
+    /**
+     * @param bool|int|string $arg
+     */
+    public function setArg(bool|int|string $arg): void
+    {
+        $this->arg = $arg;
+    }
+
+    /**
+     * @param Definer $definer
+     */
+    public function __construct(Definer $definer)
+    {
+        $this->definer = $definer;
+    }
+    public function executeStrategy(): string
+    {
+       return $this->definer->define($this->arg);
+    }
+}
+class IntDefiner implements Definer
+{
+
+    public function define($arg): string
+    {
+        return $arg . ' from int strategy';
+    }
+}
+class StringDefiner implements Definer
+{
+
+    public function define($arg): string
+    {
+        return $arg . ' from string strategy';
+    }
+}
+class BoolDefiner implements Definer
+{
+
+    public function define($arg): string
+    {
+        return $arg . ' from bool strategy';
+    }
+}
+
+$date = new  Data(new  IntDefiner());
+
+$date->setArg('some arg for first');
+
+var_dump($date->executeStrategy());
